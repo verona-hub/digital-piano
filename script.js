bed9456b76f3f227e3a72fa607c5cf4e34798b53
Zoran G <68257343+verona-hub@users.noreply.github.com>
2021-06-02 18:30:23 +0100

    Changes: - added new files (compressed and converted to .mp3) - script cleanup

--- script.js
+++ script.js
@@ -1,169 +1,170 @@
-/*jshint esversion: 6 */
-
-// Detect mouse press on all notes
-const numberOfKeyNotes = document.querySelectorAll('.notes').length;
-for (let i = 0; i < numberOfKeyNotes; i++) {
+// Get the keyboard notes size
+const notesSize = document.querySelectorAll('.notes').length;
+// Loop through all the keyboard notes
+for (let i = 0; i < notesSize; i++) {
+  // Listen for a mouse press on every note and if pressed activate the clickFunc
   document.querySelectorAll('.notes')[i].addEventListener('mousedown', clickFunc);
 }
 
-// Detect Keyboard press
+// Listen for a keyboard press on every note and if pressed activate the keydownFunc
 document.addEventListener('keydown', keydownFunc);
 
 
-
-// Function activation. Clicking on any note, the innerHTML of that particular note gets passed to the a variable 'note', which gets passed as argument to two functions: the function that makes the sound and the function that triggers the note animation;
+// Function activation on click
 function clickFunc() {
+  // Create a note variable and pass it the clicked innerHtml value
   const note = this.innerHTML;
+  // Pass the note variable as an argument to the makeSound function
   makeSound(note);
+  // Pass the note variable as a parameter to the keyAnimation function
   keyAnimation(note);
 }
 
-// Functions activation
+// Function activation on keypress
 function keydownFunc(event) {
+  // Pass the pressed key a parameter to the makeSound function
   makeSound(event.key);
+  // Pass the pressed key a parameter to the keyAnimation function
   keyAnimation(event.key);
 }
 
-
-
-
-// Animation
+// Function that triggers the note animation
 function keyAnimation(currentKey) {
+  // Create a variable activeKey and set it to be the clicked/pressed div
   const activeKey = document.querySelector('.' + currentKey);
+  // To start the animation, add the class 'pressed' to the activeKey
   activeKey.classList.add('pressed');
-
-  setTimeout(function() {
+  // To end the animation, set the duration when the 'pressed' class will be removed
+  setTimeout(() => {
     activeKey.classList.remove('pressed');
   }, 100);
 }
 
-
-
-// Switch statement with each keyboard letter assigned to a sound
-function makeSound(key) {
+// Main function that plays the sounds
+const makeSound = key => {
+  // Switch statement with each keyboard letter assigned to a note sound; pass in the clicked/pressed key
   switch (key) {
     case 'q':
-      const c4 = new Audio('sounds/1.c4.ogg');
-      c4.play();
+      const a0 = new Audio('sounds/1.a0.mp3');
+      a0.play();
       break;
 
     case 'w':
-      const c_4 = new Audio('sounds/2.c_4.ogg');
-      c_4.play();
+      const c_1 = new Audio('sounds/2.c_1.mp3');
+      c_1.play();
       break;
 
     case 'e':
-      const d4 = new Audio('sounds/3.d4.ogg');
-      d4.play();
+      const e_1 = new Audio('sounds/3.e_1.mp3');
+      e_1.play();
       break;
 
     case 'r':
-      const d_4 = new Audio('sounds/4.d_4.ogg');
-      d_4.play();
+      const a1 = new Audio('sounds/4.a1.mp3');
+      a1.play();
       break;
 
     case 't':
-      const e4 = new Audio('sounds/5.e4.ogg');
-      e4.play();
+      const c_2 = new Audio('sounds/5.c_2.mp3');
+      c_2.play();
       break;
 
     case 'y':
-      const f4 = new Audio('sounds/6.f4.ogg');
-      f4.play();
+      const e_2 = new Audio('sounds/6.e_2.mp3');
+      e_2.play();
       break;
 
     case 'u':
-      const f_4 = new Audio('sounds/7.f_4.ogg');
-      f_4.play();
+      const a2 = new Audio('sounds/7.a2.mp3');
+      a2.play();
       break;
 
     case 'i':
-      const g4 = new Audio('sounds/8.g4.ogg');
-      g4.play();
+      const c = new Audio('sounds/8.c.mp3');
+      c.play();
       break;
 
     case 'o':
-      const g_4 = new Audio('sounds/9.g_4.ogg');
-      g_4.play();
+      const e_3 = new Audio('sounds/9.e_3.mp3');
+      e_3.play();
       break;
 
     case 'p':
-      const a4 = new Audio('sounds/10.a4.ogg');
-      a4.play();
+      const a3 = new Audio('sounds/10.a3.mp3');
+      a3.play();
       break;
 
     case 'a':
-      const a_4 = new Audio('sounds/11.a_4.ogg');
-      a_4.play();
+      const c_4 = new Audio('sounds/11.c_4.mp3');
+      c_4.play();
       break;
 
     case 's':
-      const b4 = new Audio('sounds/12.b4.ogg');
-      b4.play();
+      const e_4 = new Audio('sounds/12.e_4.mp3');
+      e_4.play();
       break;
 
     case 'd':
-      const c5 = new Audio('sounds/13.c5.ogg');
-      c5.play();
+      const a4 = new Audio('sounds/13.a4.mp3');
+      a4.play();
       break;
 
     case 'f':
-      const c_5 = new Audio('sounds/14.c_5.ogg');
+      const c_5 = new Audio('sounds/14.c_5.mp3');
       c_5.play();
       break;
 
     case 'g':
-      const d5 = new Audio('sounds/15.d5.ogg');
-      d5.play();
+      const e_5 = new Audio('sounds/15.e_5.mp3');
+      e_5.play();
       break;
 
     case 'h':
-      const d_5 = new Audio('sounds/16.d_5.ogg');
-      d_5.play();
-      break;
-
-    case 'j':
-      const e5 = new Audio('sounds/17.e5.ogg');
-      e5.play();
-      break;
-
-    case 'k':
-      const f5 = new Audio('sounds/18.f5.ogg');
-      f5.play();
-      break;
-
-    case 'l':
-      const f_5 = new Audio('sounds/19.f_5.ogg');
-      f_5.play();
-      break;
-
-    case 'z':
-      const g5 = new Audio('sounds/20.g5.ogg');
-      g5.play();
-      break;
-
-    case 'x':
-      const g_5 = new Audio('sounds/21.g_5.ogg');
-      g_5.play();
-      break;
-
-    case 'c':
-      const a5 = new Audio('sounds/22.a5.ogg');
+      const a5 = new Audio('sounds/16.a5.mp3');
       a5.play();
       break;
 
+    case 'j':
+      const c_6 = new Audio('sounds/17.c_6.mp3');
+      c_6.play();
+      break;
+
+    case 'k':
+      const e_6 = new Audio('sounds/18.e_6.mp3');
+      e_6.play();
+      break;
+
+    case 'l':
+      const a6 = new Audio('sounds/19.a6.mp3');
+      a6.play();
+      break;
+
+    case 'z':
+      const c_7 = new Audio('sounds/20.c_7.mp3');
+      c_7.play();
+      break;
+
+    case 'x':
+      const c_7b = new Audio('sounds/21.c_7b.mp3');
+      c_7b.play();
+      break;
+
+    case 'c':
+      const e_7 = new Audio('sounds/22.e_7.mp3');
+      e_7.play();
+      break;
+
     case 'v':
-      const a_5 = new Audio('sounds/23.a_5.ogg');
-      a_5.play();
+      const a7 = new Audio('sounds/23.a7.mp3');
+      a7.play();
       break;
 
     case 'b':
-      const b5 = new Audio('sounds/24.b5.ogg');
-      b5.play();
+      const c8 = new Audio('sounds/24.c8.mp3');
+      c8.play();
       break;
 
-      // Default left empty on purpose so the user is free to use keyboard shortcuts without interfering with the switch statement (example: fn + random key)
-      // default:
-
+    default: // Default left empty on purpose so the user is free to use keyboard shortcuts without interfering with the switch statement (example: fn + key)
+      break;
   }
 }
